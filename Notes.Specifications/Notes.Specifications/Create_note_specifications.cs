5696ade81a850a4dcdc278d4b9ce395045aacb06
Robyn Le Sueur <robyn.lesueur@googlemail.com>
2013-08-26 20:12:54 +0100

    Began driving out application.

---
+++ Notes.Specifications/Notes.Specifications/Create_note_specifications.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using FluentAssertions;
+using Notes.Web.Controllers;
+using NUnit.Framework;
+
+namespace Notes.Specifications
+{
+    [TestFixture]
+    public class Create_note_specifications
+    {
+        [Test]
+        public void Can_create_a_note_specification()
+        {
+            // Arrange
+
+            // Act
+            var controllerUnderTest = new NoteController();
+            var result = controllerUnderTest.CreateNote() as ViewResult;
+            var model = result.Model as NoteModel;
+
+            // Assert
+            model.Should().BeOfType<NoteModel>();
+        }
+    }
+
+    public class NoteModel
+    {
+    }
+}
